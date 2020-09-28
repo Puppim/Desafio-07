3d78e4120794bc8771c41be5b78df1293bb7c455
Diogo Puppim de Oliveira <diogopuppim@gmail.com>
2020-09-28 17:33:16 -0300

    Solucao 01 desafio07

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,9 @@
+const dateFormat = (value: Date): string => {
+  const data = new Date(value);
+  const dia = data.getDate().toString().padStart(2, '0');
+  const mes = (data.getMonth() + 1).toString().padStart(2, '0');
+  const ano = data.getFullYear();
+  return `${dia}/${mes}/${ano}`;
+};
+
+export default dateFormat;
